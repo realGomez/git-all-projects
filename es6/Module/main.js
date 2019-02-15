3242013f81eb5a23e3e1502f4cbcc7a42371735f
gomez.wong <gomez.wong@bcnetcom.com>
2019-02-15 16:06:22 +0800

    module

---
+++ es6/Module/main.js
@@ -0,0 +1,9 @@
+import { lastName as surname } from './profile.js';
+
+// function setName() {
+//     console.log(firstName,lastName,birtdday);
+// }
+//
+// setName();
+
+console.log(surname);
---
+++ es6/Module/profile.js
@@ -0,0 +1,5 @@
+var firstName = "Gomez";
+var lastName = "Wong";
+var birthday = "1993.05.26";
+
+export {firstName,lastName,birthday};
