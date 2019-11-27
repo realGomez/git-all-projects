5e48e23ff7c6f3da4717c1d3164b944e34d083e6
gomez <772003394@qq.com>
2019-11-28 00:22:23 +0800

    vue

---
+++ vue-bog/blog/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <add-blog />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import addBlog from '@/components/addBlog.vue'
+
+export default {
+  name: 'home',
+  components: {
+    addBlog
+  }
+}
+</script>
