e18637683ed57dd1f781bc645bbcb628b2d44c10
gomez.wong <gomez.wong@bcnetcom.com>
2019-11-29 18:15:40 +0800

    fix:vue

--- vue-blog/blog/src/views/Home.vue
+++ vue-blog/blog/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <add-blog />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import addBlog from '@/components/addBlog.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    addBlog
   }
 }
 </script>
