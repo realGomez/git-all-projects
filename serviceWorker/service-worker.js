010450178281d4b9d0300ef1fe301a1e875317e7
Gomez <gomez.wong@bcnetcom.com>
2024-05-30 09:24:20 +0800

    fix:service-worker

--- serviceWorker/service-worker.js
+++ serviceWorker/service-worker.js
@@ -8,51 +8,53 @@ self.addEventListener('install', event => {
 self.addEventListener('install', (event) => {
   let CACHE_NAME = 'xyz-cache'
   let urlsToCache = [
-      '/',
-      '/test.js'
+    '/serviceWorker/index.html',
+    '/serviceWorker/main.css',
+    '/serviceWorker/icon.png',
   ]
+
   event.waitUntil(
-      caches.open(CACHE_NAME)
-      .then (cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   )
 })
 
 self.addEventListener('activate', (event) => {
   let cacheWhitelist = ['products-v2']
   event.waitUntil(
-      caches.keys().then (cacheNames => {
-          return Promise.all(
-              cacheNames.map( cacheName => {
-                  if (cacheWhitelist.indexOf(cacheName) === -1) {
-                      return caches.delete(cacheName)
-                  }
-              })
-          )
-      })
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
   )
 })
 
-console.log('activate d self',self);
+console.log('activate d self', self);
 
 self.addEventListener('activate', (event) => {
 
 
   event.waitUntil(
-      self.clients.matchAll().then ( 
-        (client) => {
+    self.clients.matchAll().then(
+      (client) => {
 
-          console.log('activate client',client);
+        console.log('activate client', client);
 
-          client.postMessage({
-              msg: 'Hey, from service worker! I\'m listening to your fetch requests.',
-              source: 'service-worker'
-          })
+        client.postMessage({
+          msg: 'Hey, from service worker! I\'m listening to your fetch requests.',
+          source: 'service-worker'
+        })
       })
   )
 })
 
 self.addEventListener('fetch', (event) => {
-  event.waitUntil(()=>{
+  event.waitUntil(() => {
     if (event.request.url.includes('/product')) {
       let productId = event.data.productId
       let productCount = getProductData(productId)
@@ -63,4 +65,10 @@ self.addEventListener('fetch', (event) => {
     }
   })
 })
+
+self.addEventListener('message', (event) => {
+  console.log('页面传递过来的数据', event.data)  // 收到主线程传递的信息
+  event.source.postMessage('this message is from sw.js to page');  // 向主线程传递信息
+})
+
 
